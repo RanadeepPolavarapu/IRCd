42110fb12f6b558981c551fb3bebf5f2f7f4b021
RanadeepPolavarapu <RanadeepPolavarapu@users.noreply.github.com>
2016-02-17 17:37:07 -0500

    add gmake in prereqs shell script

--- prereqs.sh
+++ prereqs.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+echo "[INFO]: Installing libssl-dev for UnrealIRCd SSL connections"
 apt-get -y install libssl-dev
+
+echo "[INFO]: Installing gmake for building Anope services"
+apt-get -y install gmake
